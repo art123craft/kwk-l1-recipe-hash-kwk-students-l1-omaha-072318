5cd596ce413c48432eaa65588c08e6f9e5dcdd68
art123craft <cammiebluecammie@gmail.com>
2018-07-26 20:35:54 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -1,10 +1,10 @@
 #code your solutions here
 perfect_10_recipe = {
   :almond_flour => "2 cups", 
-  :gluten_free_whole_oats => "1 cups", 
-  :kosher_salt => "1/2 teaspoon", 
-  :baking_powder => "1/2 teaspoon", 
-  :xanthan_gum => "1/2 teaspoon", 
+  :gluten_free_whole_oats => "1 cup", 
+  :kosher_salt => "1/2 tea spoon", 
+  :baking_powder => "1/2 tea spoon", 
+  :xanthan_gum => "1/2 tea spoon", 
   :slivered_almonds => "1/4 cup", 
   :mini_dark_chocolate_chips => "3/4 cup", 
   :olive_oil => "1/2 cup", 

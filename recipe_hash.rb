8c89297f50e087b502dae1b0b47a5b0fca3cfd35
art123craft <cammiebluecammie@gmail.com>
2018-07-26 20:45:26 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -2,8 +2,9 @@
 perfect_10_recipe = {
   :almond_flour => "2 cups", 
   :gluten_free_whole_oats => "1 cup", 
-  :kosher_salt => "1/2 tea spoon", 
+  :kosher_salt => "1/2 tea spoon",
   :baking_powder => "1/2 tea spoon", 
+  :bakin_soda => "1/4 tea spoon",
   :xanthan_gum => "1/2 tea spoon", 
   :slivered_almonds => "1/4 cup", 
   :mini_dark_chocolate_chips => "3/4 cup", 
